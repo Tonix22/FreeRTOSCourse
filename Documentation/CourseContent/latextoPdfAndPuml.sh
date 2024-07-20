64412cc49f885606a84f2a2207f632fe0764bcd5
Tonix22 <emiliotonix@gmail.com>
2024-07-20 01:13:29 -0600

    partial commit before doing Json sync strategy

--- Documentation/CourseContent/latextoPdfAndPuml.sh
+++ Documentation/CourseContent/latextoPdfAndPuml.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-
+./renderpuml.sh
 # Constants
-INPUT_FILE="CourseContent.lyx"
+INPUT_FILE="CourseContent"
 OUTPUT_FILE="CourseContent.pdf"
 OUTPUT_DIR="$(dirname "$OUTPUT_FILE")"
 
-# Export .lyx to .tex
-lyx -e pdflatex "$INPUT_FILE"
-
-# Get the filename without extension
-BASE_NAME=$(basename "$INPUT_FILE" .lyx)
 
 # Ensure the output directory exists
 mkdir -p "$OUTPUT_DIR"
 
 # Compile .tex to PDF using pdflatex
-pdflatex -interaction=nonstopmode -output-directory="$OUTPUT_DIR" "$OUTPUT_DIR/${BASE_NAME}.tex"
+pdflatex -interaction=nonstopmode -output-directory="$OUTPUT_DIR" "$OUTPUT_DIR/${INPUT_FILE}.tex"
 
 # Rename the output PDF to the desired name if not already correct
 mv "$OUTPUT_DIR/${BASE_NAME}.pdf" "$OUTPUT_FILE"
