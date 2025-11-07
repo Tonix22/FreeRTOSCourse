e7bfbcd97e635d3b8848bb5e53e3601751fa7341
Tonix22 <emiliotonix@gmail.com>
2025-11-07 11:28:29 -0600

    add main

--- Projects/TaskCreateActivity/main/main.cpp
+++ Projects/TaskCreateActivity/main/main.cpp
@@ -22,15 +22,91 @@
 #include "taskWrapper.h"
 #include "ledtask.h"
 #include "freertos/FreeRTOS.h"
+//#include "freertos/semaphore.h"
 #include "delaytask.h"
 
 using namespace std;
+SemaphoreHandle_t xSemaphore;// use this sempahore as a controler between task
+QueueHandle_t xQueue;
 
+typedef struct twoValues
+{
+  long real;
+  long imaginary;
+}ComplexData;
+
+//void* is a generic data type pointer
+void vAlice(void *pvParameters)
+{
+  int UART_ready = 10;
+  int cnt = 0;
+  ComplexData complexValue;
+  complexValue.real = 777;
+  complexValue.imaginary = 888;
+  
+  xQueueSend (xQueue , &complexValue , 0);
+  //xSemaphoreTake(xSemaphore, portMAX_DELAY);
+  //take until Uart is done
+  for(;;)
+  {
+    vTaskDelay(5000/portTICK_PERIOD_MS);
+    complexValue.real++;
+    complexValue.imaginary++;
+    xQueueSend (xQueue , &complexValue , 0);
+    //if(cnt == UART_ready)
+    //{
+    //  xSemaphoreGive(xSemaphore);
+    //}
+  }
+}
+
+void vBob(void *pvParameters)
+{
+  //xSemaphoreTake(xSemaphore, portMAX_DELAY);
+  ComplexData recievedComplex;
+  for(;;)
+  {
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+    xQueueReceive(xQueue , & recievedComplex , portMAX_DELAY );
+    printf("Complex value: r:%ld,i:%ld\n",recievedComplex.real,recievedComplex.imaginary);
+    //vTaskDelay(2000/portTICK_PERIOD_MS);
+  }
+}
 
 // Main function
 extern "C" void app_main()
 {
-    // Create LED task
+  int* bobint = (int*)malloc(sizeof(int));// ask for one element
+  *bobint = 3; //change value to that element
+
+  xSemaphore = xSemaphoreCreateBinary();
+  xQueue = xQueueCreate(3,sizeof(ComplexData));//160 bytes
+  if(xQueue == NULL)
+  {
+    printf("Queue creation fail\n");
+  }
+
+
+  if(xSemaphore == NULL)
+  {
+    printf("We dont have heap space\n");
+    //heap space dynamic memory allocation
+    //dynamic alloaction wasn't possible
+  }
+  else
+  {
+    xSemaphoreGive(xSemaphore);
+  }
+  
+  xTaskCreate(vAlice,"Alice",4096,NULL,3,NULL);
+  xTaskCreate(vBob,"Bob",4096,bobint,2,NULL);//share 3 to the task
+  
+  while (1) 
+  {
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
+  }
+  // Create LED task
     /*
     LedTask ledTask1("LED Task 1", 4096, 3, GPIO_NUM_32, pdMS_TO_TICKS(2000)); // Blinks LED on pin 2 every 500ms
     ledTask1.start();
@@ -42,15 +118,11 @@ extern "C" void app_main()
     ledTask3.start();
     */
 
-    DelayTask delay1(1000,"Delay Task 1");
-    DelayTask delay2(2000,"Delay Task 2");
+    //DelayTask delay1(1000,"Delay Task 1");
+    //DelayTask delay2(2000,"Delay Task 2");
 
-    delay1.start();
-    delay2.start();
+    //delay1.start();
+    //delay2.start();
 
-      while (1) {
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-        //delay.InMs(100);
-    }
   
 }
